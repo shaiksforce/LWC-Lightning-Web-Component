a3cbd6b52b2c5fb771e41f2fd3a948746f47d989
Shaik Nagajani <32892699+shaiksforce@users.noreply.github.com>
2019-07-06 11:30:14 +0530

    Update deleteRowsInDatatableLWC.js

--- Delete Multiple Rows In Datatable/deleteRowsInDatatableLWC.js
+++ Delete Multiple Rows In Datatable/deleteRowsInDatatableLWC.js
@@ -1,40 +1,51 @@
-import {LightningElement, wire, track} from 'lwc';
+import {LightningElement, track, wire} from 'lwc';
 
 // importing apex class methods
 import getContacts from '@salesforce/apex/LWCExampleController.getContacts';
+import delSelectedCons from '@salesforce/apex/LWCExampleController.deleteContacts';
 
-// datatable columns with row actions
+// importing to show toast notifictions
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+// importing to refresh the apex after delete the records.
+import {refreshApex} from '@salesforce/apex';
+
+// datatable columns
 const columns = [
     {
         label: 'FirstName',
-        fieldName: 'FirstName',
-        sortable: "true"
+        fieldName: 'FirstName'
     }, {
         label: 'LastName',
-        fieldName: 'LastName',
-        sortable: "true"
+        fieldName: 'LastName'
     }, {
         label: 'Phone',
         fieldName: 'Phone',
-        type: 'phone',
-        sortable: "true"
+        type: 'phone'
     }, {
         label: 'Email',
         fieldName: 'Email',
         type: 'email'
-    },
+    }
 ];
 
-export default class DataTableWithSortingInLWC extends LightningElement { 
+export default class DeleteRowsInDatatableLWC extends LightningElement {
     // reactive variable
     @track data;
     @track columns = columns;
-    @track sortBy;
-    @track sortDirection;
+    @track buttonLabel = 'Delete Selected Contacts';
+    @track isTrue = false;
+    @track recordsCount = 0;
+
+    // non-reactive variables
+    selectedRecords = [];
+    refreshTable;
+    error;
 
     // retrieving the data using wire service
     @wire(getContacts)
     contacts(result) {
+        this.refreshTable = result;
         if (result.data) {
             this.data = result.data;
             this.error = undefined;
@@ -45,41 +56,78 @@ export default class DataTableWithSortingInLWC extends LightningElement {
         }
     }
 
-    handleSortdata(event) {
-        // field name
-        this.sortBy = event.detail.fieldName;
 
-        // sort direction
-        this.sortDirection = event.detail.sortDirection;
+    // Getting selected rows 
+    getSelectedRecords(event) {
+        // getting selected rows
+        const selectedRows = event.detail.selectedRows;
+        
+        this.recordsCount = event.detail.selectedRows.length;
 
-        // calling sortdata function to sort the data based on direction and selected field
-        this.sortData(event.detail.fieldName, event.detail.sortDirection);
+        // this set elements the duplicates if any
+        let conIds = new Set();
+
+        // getting selected record id
+        for (let i = 0; i < selectedRows.length; i++) {
+            conIds.add(selectedRows[i].Id);
+        }
+
+        // coverting to array
+        this.selectedRecords = Array.from(conIds);
+
+        window.console.log('selectedRecords ====> ' +this.selectedRecords);
     }
 
-    sortData(fieldname, direction) {
-        // serialize the data before calling sort function
-        let parseData = JSON.parse(JSON.stringify(this.data));
 
-        // Return the value stored in the field
-        let keyValue = (a) => {
-            return a[fieldname];
-        };
+    // delete records process function
+    deleteAccounts() {
+        if (this.selectedRecords) {
+            // setting values to reactive variables
+            this.buttonLabel = 'Processing....';
+            this.isTrue = true;
 
-        // cheking reverse direction 
-        let isReverse = direction === 'asc' ? 1: -1;
+            // calling apex class to delete selected records.
+            this.deleteCons();
+        }
+    }
 
-        // sorting data 
-        parseData.sort((x, y) => {
-            x = keyValue(x) ? keyValue(x) : ''; // handling null values
-            y = keyValue(y) ? keyValue(y) : '';
 
-            // sorting values based on direction
-            return isReverse * ((x > y) - (y > x));
+    deleteCons() {
+        delSelectedCons({lstConIds: this.selectedRecords})
+        .then(result => {
+            window.console.log('result ====> ' + result);
+
+            this.buttonLabel = 'Delete Selected Contacts';
+            this.isTrue = false;
+
+            // showing success message
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success!!', 
+                    message: this.recordsCount + ' Contacts are deleted.', 
+                    variant: 'success'
+                }),
+            );
+            
+            // Clearing selected row indexs 
+            this.template.querySelector('lightning-datatable').selectedRows = [];
+
+            this.recordsCount = 0;
+
+            // refreshing table data using refresh apex
+            return refreshApex(this.refreshTable);
+
+        })
+        .catch(error => {
+            window.console.log(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error while getting Contacts', 
+                    message: error.message, 
+                    variant: 'error'
+                }),
+            );
         });
-
-        // set the sorted data to data table data
-        this.data = parseData;
-
-    }
+    }  
 
 }
