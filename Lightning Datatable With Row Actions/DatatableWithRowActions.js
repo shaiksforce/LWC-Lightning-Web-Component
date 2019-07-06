7487c9179944afa656e83c0bc56376472dc2d4d3
Shaik Nagajani <32892699+shaiksforce@users.noreply.github.com>
2019-07-06 10:48:44 +0530

    Create DatatableWithRowActions.js

---
+++ Lightning Datatable With Row Actions/DatatableWithRowActions.js
@@ -0,0 +1,165 @@
+import {LightningElement, track, wire} from 'lwc';
+
+// importing apex class methods
+import getContacts from '@salesforce/apex/LWCExampleController.getContacts';
+import delSelectedCons from '@salesforce/apex/LWCExampleController.deleteContacts';
+
+// importing to show toast notifictions
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+// importing to refresh the apex if any record changes the datas
+import {refreshApex} from '@salesforce/apex';
+
+// row actions
+const actions = [
+    { label: 'Record Details', name: 'record_details'}, 
+    { label: 'Edit', name: 'edit'}, 
+    { label: 'Delete', name: 'delete'}
+];
+
+// datatable columns with row actions
+const columns = [
+    { label: 'FirstName', fieldName: 'FirstName' }, 
+    { label: 'LastName', fieldName: 'LastName' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone'}, 
+    { label: 'Email', fieldName: 'Email', type: 'email' }, 
+    {
+        type: 'action',
+        typeAttributes: {
+            rowActions: actions,
+            menuAlignment: 'right'
+        }
+    }
+];
+
+export default class DeleteRowsInDatatableLWC extends LightningElement { 
+    // reactive variable
+    @track data;
+    @track columns = columns;
+    @track record = [];
+    @track bShowModal = false;
+    @track currentRecordId;
+    @track isEditForm = false;
+    @track showLoadingSpinner = false;
+
+    // non-reactive variables
+    selectedRecords = [];
+    refreshTable;
+    error;
+
+    // retrieving the data using wire service
+    @wire(getContacts)
+    contacts(result) {
+        this.refreshTable = result;
+        if (result.data) {
+            this.data = result.data;
+            this.error = undefined;
+
+        } else if (result.error) {
+            this.error = result.error;
+            this.data = undefined;
+        }
+    }
+
+    handleRowActions(event) {
+        let actionName = event.detail.action.name;
+
+        window.console.log('actionName ====> ' + actionName);
+
+        let row = event.detail.row;
+
+        window.console.log('row ====> ' + row);
+        // eslint-disable-next-line default-case
+        switch (actionName) {
+            case 'record_details':
+                this.viewCurrentRecord(row);
+                break;
+            case 'edit':
+                this.editCurrentRecord(row);
+                break;
+            case 'delete':
+                this.deleteCons(row);
+                break;
+        }
+    }
+
+    // view the current record details
+    viewCurrentRecord(currentRow) {
+        this.bShowModal = true;
+        this.isEditForm = false;
+        this.record = currentRow;
+    }
+
+    // closing modal box
+    closeModal() {
+        this.bShowModal = false;
+    }
+
+
+    editCurrentRecord(currentRow) {
+        // open modal box
+        this.bShowModal = true;
+        this.isEditForm = true;
+
+        // assign record id to the record edit form
+        this.currentRecordId = currentRow.Id;
+    }
+
+    // handleing record edit form submit
+    handleSubmit(event) {
+        // prevending default type sumbit of record edit form
+        event.preventDefault();
+
+        // querying the record edit form and submiting fields to form
+        this.template.querySelector('lightning-record-edit-form').submit(event.detail.fields);
+
+        // closing modal
+        this.bShowModal = false;
+
+        // showing success message
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Success!!',
+            message: event.detail.fields.FirstName + ' '+ event.detail.fields.LastName +' Contact updated Successfully!!.',
+            variant: 'success'
+        }),);
+
+    }
+
+    // refreshing the datatable after record edit form success
+    handleSuccess() {
+        return refreshApex(this.refreshTable);
+    }
+
+    deleteCons(currentRow) {
+        let currentRecord = [];
+        currentRecord.push(currentRow.Id);
+        this.showLoadingSpinner = true;
+
+        // calling apex class method to delete the selected contact
+        delSelectedCons({lstConIds: currentRecord})
+        .then(result => {
+            window.console.log('result ====> ' + result);
+            this.showLoadingSpinner = false;
+
+            // showing success message
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Success!!',
+                message: currentRow.FirstName + ' '+ currentRow.LastName +' Contact deleted.',
+                variant: 'success'
+            }),);
+
+            // refreshing table data using refresh apex
+             return refreshApex(this.refreshTable);
+
+        })
+        .catch(error => {
+            window.console.log('Error ====> '+error);
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error!!', 
+                message: error.message, 
+                variant: 'error'
+            }),);
+        });
+    }
+
+}
